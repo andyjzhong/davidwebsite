26d15d124639b0af883a8745e4af84c997054a75
Andy Zhong <andyjzhong@gmail.com>
2018-02-03 17:28:26 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import logo from './logo.svg';
+import fish from './koifish.jpg';
+import './App.css';
+import MuiThemeProvider from '../node_modules/material-ui/styles/MuiThemeProvider';
+import AppBar from './components/app_bar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <h1 className="App-title">David Zhong</h1>
+        </header>
+        <p className="App-intro">
+          Welcome to my personal webpage!
+        </p>
+        <img src={fish} className="Koi-fish" alt="fish" />
+      </div>
+    );
+  }
+}
+
+export default App;
