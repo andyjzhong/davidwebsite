e76c7b39555639d16874912a1798748e33ac5529
Andy Zhong <andyjzhong@gmail.com>
2018-02-03 17:41:07 -0500

    Added DJZ logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import logo from './logo.svg';
+import initials from './initials.png';
 import fish from './koifish.jpg';
 import './App.css';
 import MuiThemeProvider from '../node_modules/material-ui/styles/MuiThemeProvider';
@@ -12,12 +13,15 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-
           <h1 className="App-title">David Zhong</h1>
         </header>
+        <br/>
+        <img src={initials} className="initials" alt="initials" />
+        <br/>
         <p className="App-intro">
           Welcome to my personal webpage!
         </p>
+
         <img src={fish} className="Koi-fish" alt="fish" />
       </div>
     );
