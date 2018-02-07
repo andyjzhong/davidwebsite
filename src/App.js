69dd449b9814e01af3350442265066274c4b09b9
Andy Zhong <andyjzhong@gmail.com>
2018-02-06 20:38:57 -0500

    Revamped website with html and css content

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import initials from './initials.png';
-import fish from './koifish.jpg';
+import photo from './photo.png';
 import './App.css';
 import MuiThemeProvider from '../node_modules/material-ui/styles/MuiThemeProvider';
 import Button from './components/button';
@@ -11,23 +11,26 @@ import Navbar from './components/navbar';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">David Zhong</h1>
+      <div>
+        <head>
+        </head>
+        <body>
           <Navbar />
-        </header>
-        <br/>
+          <div class="namePlate">
+            <img src={photo} class="photo" alt="photo" />
+            <h1 class="text-center">David J. Zhong</h1>
+            <p class="text-center">Chemical Engineering Student</p>
+          </div>
 
-        <img src={initials} className="initials" alt="initials" />
-        <br/>
 
-        <p className="App-intro">
-          Welcome to my personal webpage!
-        </p>
 
-        <img src={fish} className="Koi-fish" alt="fish" />
-        <Button />
+          <ul class="text-center container">
+            <li><a href="">About</a></li>
+            <li><a href="">Work</a></li>
+            <li><a href="mailto:d.j.zhongbls@gmail.com">Contact</a></li>
+          </ul>
+          <footer>2018</footer>
+        </body>
       </div>
     );
   }
